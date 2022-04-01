ef9985e9162bc9662285a084ba992a5637801190
Dian Tri Budianto <dyanzzz@gmail.com>
2022-04-01 10:38:18 +0700

    remove authentication in path songs & albums

--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -3,41 +3,26 @@ const routes = (handler) => [
     method: 'POST',
     path: '/songs',
     handler: handler.postSongHandler,
-    options: {
-      auth: 'openmusicapp_jwt',
-    },
   },
   {
     method: 'GET',
     path: '/songs',
     handler: handler.getSongsHandler,
-    options: {
-      auth: 'openmusicapp_jwt',
-    },
   },
   {
     method: 'GET',
     path: '/songs/{id}',
     handler: handler.getSongByIdHandler,
-    options: {
-      auth: 'openmusicapp_jwt',
-    },
   },
   {
     method: 'PUT',
     path: '/songs/{id}',
     handler: handler.putSongByIdHandler,
-    options: {
-      auth: 'openmusicapp_jwt',
-    },
   },
   {
     method: 'DELETE',
     path: '/songs/{id}',
     handler: handler.deleteSongByIdHandler,
-    options: {
-      auth: 'openmusicapp_jwt',
-    },
   },
 ];
 
